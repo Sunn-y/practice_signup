a0d4a35000f7a8a658051d7d7bde06f2f5535c9c
Sunn-y <in.wise.nature@gmail.com>
2020-11-11 22:47:13 +0900

    signin with EventBus

---
+++ src/components/Users/AllUsers.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+	<h1>All Users</h1>
+	<v-list two-line>
+		<v-list-tile v-for="(user, index) in allUsers" :key="index" avatar>
+			<v-list-tile-avatar color="grey lighten-3">
+				<v-img v-bind:src="user.src"></v-img>
+			</v-list-tile-avatar>
+
+			<v-list-tile-content>
+				<v-list-tile-title v-html="user.name"></v-list-tile-title>
+				<v-list-tile-sub-title>id:#{{index}} / From {{user.address}}</v-list-tile-sub-title>
+			</v-list-tile-content>
+		</v-list-tile>
+	</v-list>
+
+</div>
+</template>
+
+<script>
+import {
+	EventBus
+} from '@/main.js'
+
+export default {
+	data() {
+		return {
+			allUsers: [{
+					userId: 'mike00',
+					password: '123',
+					name: 'Mike',
+					address: 'France',
+					src: 'https://www.flaticon.com/svg/static/icons/svg/145/145867.svg'
+				},
+				{
+					userId: 'jane00',
+					password: '456',
+					name: 'Jane',
+					address: 'Spain',
+					src: 'https://www.flaticon.com/svg/static/icons/svg/145/145866.svg'
+				},
+				{
+					userId: 'alice00',
+					password: '789',
+					name: 'Alice',
+					address: 'Portugal',
+					src: 'https://www.flaticon.com/svg/static/icons/svg/145/145852.svg'
+				}
+			]
+		}
+	},
+	mounted() {
+		EventBus.$on('signUp', users => {
+			this.allUsers.push(users)
+		})
+	}
+}
+</script>
---
+++ src/components/Users/SignUp.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+	<h1>SignUp</h1>
+	<v-text-field label="userID" v-model="userId"></v-text-field>
+	<v-text-field label="password" type="password" v-model="password"></v-text-field>
+	<v-text-field label="userName" v-model="name"></v-text-field>
+	<v-text-field label="Address" v-model="address"></v-text-field>
+	<v-text-field label="Avatar" v-model="src"></v-text-field>
+	<v-btn block color="cyan" @click="signUp">Sign Up</v-btn>
+</div>
+</template>
+
+<script>
+import {
+	EventBus
+} from '@/main.js'
+
+export default {
+	data() {
+		return {
+			userId: null,
+			password: null,
+			name: null,
+			address: null,
+			src: null
+		}
+	},
+	methods: {
+		signUp() {
+			let userObj = {
+				userId: this.userId,
+				password: this.password,
+				name: this.name,
+				address: this.address,
+				src: this.src
+			}
+			EventBus.$emit('signUp', userObj)
+			this.clearForm()
+		},
+		clearForm() {
+			this.userId = null,
+				this.password = null,
+				this.name = null,
+				this.address = null,
+				this.src = null
+		}
+	}
+}
+</script>
